aae78b97dcd5c57e34bc756078960908ab0f368e
Pritam Saha <saha7pritam@gmail.com>
2024-05-30 21:41:12 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Saha-7",
   resume:
-    "",
+    "https://drive.google.com/file/d/1K0TpaX_rnKsoz0a_MoXws0AwnlXagvFl/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/pritamsaha7/",
   twitter: "https://twitter.com/saha7pritam",
   insta: "https://www.instagram.com/ig_saha7/",
