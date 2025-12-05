6ac7c061d33c3fd1b607c536b6dd5ec02b029673
Pritam Saha <saha7pritam@gmail.com>
2025-12-05 18:44:27 +0530

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- src/data/constants.js
+++ src/data/constants.js
@@ -169,7 +169,7 @@ export const education = [
     id: 0,
     img: "https://th.bing.com/th/id/OIP.-2NAJRdcMVIksOMROTNfKAHaHa?w=129&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7",
     school: "Adamas University,  Kolkata",
-    date: "2018 - 2022",
+    date: "",
     grade: "8.1 CGPA",
     desc: "I have completed a Bachelor's degree in Computer Science and Engineering at Adamas University, Kolkata.",
     degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
@@ -257,6 +257,7 @@ export const projects = [
     webapp: "https://course-selling-app-back-end.onrender.com",
   } 
 ];
+
 
 
 
